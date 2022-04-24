d31d089eeccce6cf8c48613526e06d53ef0c6cc8
Mwagiru <joymwagiru@gmail.com>
2022-04-24 12:35:56 +0300

    Completed Assignmnet

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementsByTagName('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const list = document.createElement('li')
+    list.textContent = e.target.new_task_description.value
+
+    const ul = document.getElementById('tasks')
+    ul.appendChild(li)
+
+    form.reset()
+
+  })
 });
